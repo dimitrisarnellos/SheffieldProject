04c6a554ca07d6920652490489284e2ec1b4d435
D. Arnellos <dimitrios.arnellos.211@student.lu.se>
2017-05-08 12:55:37 +0100

    Update scripts and add IBDseq.sh

--- fixmaps.py
+++ fixmaps.py
@@ -2,24 +2,27 @@ import sys, re
 
 '''
 Author: Dimitris Arnellos
-Purpose: Renames variant ID of one plink map file based on another map file for the same position
+Purpose: Renames variant ID of one plink map file based on another map file for the same position. Rest
+	of snps become junk and are annotated as "rs000".
 Usage: join -j 4 <(sort -k4,4 my.map) <(sort -k4,4 reference.map) -o '1.1,2.2,1.3,2.4' > correctRS.map
 	python fixmaps.py correctRS.map mapToBeChanged > changedMap
 '''
 
 ref = [[],[]]
+refpos = []
 with open(sys.argv[1], 'r') as f:
 	for line in f:
 		fields = re.match('\S+\s+(\S+)\s+\S+\s+(\S+)\s+', line)
 		ref[0].append(fields.group(1))
 		ref[1].append(fields.group(2))
+		refpos.append(fields.group(2))
+refpos = set(refpos)
 #print(ref)
 
 with open(sys.argv[2], 'r') as f:
 	for line in f:
 		fields = re.match('(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+', line)
-		if fields.group(4) in ref[1]:
+		if fields.group(4) in refpos:
 			print(fields.group(1), ref[0][ref[1].index(fields.group(4))], fields.group(3), fields.group(4), sep="\t")
 		else:
-			fields = fields.group(0).rstrip()
-			print(fields)
+			print(fields.group(1), "rs000", fields.group(3), fields.group(4), sep="\t")
--- runBeagle.sh
+++ runBeagle.sh
@@ -9,4 +9,4 @@
 
 module load apps/binapps/java/1.8.0u112
 export _JAVA_OPTIONS='-Xmx18G'
-java -Xss5m -jar /home/bo4da/Programs/bin/beagle.21Jan17.6cc.jar gt=/fastdata/bo4da/Data/Merged/Chr${chromosome}/CHR${chromosome}DenNeaHumChimp.vcf map=/home/bo4da/Analyses/BEAGLE/mapFiles/plink.chr${chromosome}.GRCh37.map ibd=true impute=false ibdcm=0.001 niterations=0 ibdtrim=0 out=chr${chromosome}
+java -Xss5m -jar /home/bo4da/Programs/bin/beagle.21Jan17.6cc.jar gt=/fastdata/bo4da/Data/Chimpanzee/cnagPaper/allIndivs/Chr${chromosome}/CHR${chromosome}DenNeaHumChimp.vcf map=/home/bo4da/Analyses/BEAGLE/mapFiles/plink.chr${chromosome}.GRCh37.map ibd=true impute=false ibdcm=0.001 niterations=0 ibdtrim=0 out=chr${chromosome}

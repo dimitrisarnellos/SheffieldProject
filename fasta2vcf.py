5a755c95002068cda7cea348b32939dca93496a1
Dimitrios Arnellos <dimiarn@au220.int>
2021-04-25 01:22:49 +0200

    Make fasta2vcf actually work

--- fasta2vcf.py
+++ fasta2vcf.py
@@ -1,46 +1,56 @@
 '''
 Author: Dimitris Arnellos
-Usage: python3 fasta2vcf.py sequence referenceSequence > file.vcf
+Usage: python3 fasta2vcf.py -f fasta_sequence reference_fasta_sequence -i indivs_name> file.vcf
 '''
 
-import sys, re
+import sys
+import re
+import argparse
+
+parser = argparse.ArgumentParser(description='Convert a fasta file to VCF using another reference fasta.')
+parser.add_argument('-f', '--fasta', help='Fasta for conversion', required=True)
+parser.add_argument('-r', '--reference', help='Reference fasta', required=True)
+parser.add_argument('-i', '--indiv', help='Individuals name from fasta', required=True)
+args = parser.parse_args()
 
 seq = []
-with open(sys.argv[1], 'r') as f:
+with open(args.fasta, 'r') as f:
 	for line in f:
 		if re.match('\>', line):
 			chr = line.strip(">")
 			chr = chr.strip()
 		else:
 			line = line.strip()
-			seq = list(line)
+			for char in line:
+				seq.append(char)
 
 ref = []
-with open(sys.argv[2], 'r') as f:
+with open(args.reference, 'r') as f:
 	for line in f:
 		if re.match('\>', line):
 			continue
 		else:
 			line = line.strip()
-			ref = list(line)
+			for char in line:
+				ref.append(char)
 
-
-print("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tChimpanzee")
+print(f"#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{args.indiv}")
 
 for i in range(0, len(seq)):
 
 	alt = ""
 	gt = ""
-	#line = chr + "\t" + str(i + 1) + "\t" + "." + "\t" + ref[i].upper() + "\t" + alt + "\t" + "." + "\t" + "." + "\t" + "." + "\t" + "GT" + gt
 
-	if (seq[i].upper() == "-" or ref[i].upper() == "N"):
+	if (seq[i].upper() == "N" or ref[i].upper() == "N"):
 		alt = "."
 		gt = ".|."
+		continue
 	elif (ref[i].upper() == seq[i].upper()):
 		alt = "."
 		gt = "0|0"
 	elif (ref[i].upper() != seq[i].upper()):
 		alt = seq[i].upper()
 		gt = "1|1"
+
 	line = chr + "\t" + str(i + 1) + "\t" + "." + "\t" + ref[i].upper() + "\t" + alt + "\t" + "." + "\t" + "." + "\t" + "." + "\t" + "GT" + "\t" + gt
 	print(line)
